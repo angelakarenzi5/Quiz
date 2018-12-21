0abeb7234141fc8a6e2bd95dba88bc3e809ef33e
Angela Uwase <angelakarenzi5@gmail.com>
2018-12-21 13:05:14 +0200

    added links

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+var answers = ["D", "A", "C", "A", "C", "B", "D", "A", "D", "B"],
+  tot = answers.length;
+
+function getCheckedValue(radioName) {
+  var radios = document.getElementsByName(radioName); // Get radio group by-name
+  for (var y = 0; y < radios.length; y++) {
+    if (radios[y].checked) return radios[y].value; // return the checked value
+  }
+}
+
+function getScore() {
+  var score = 0;
+  debugger;
+  for (var i = 0; i < tot; i++)
+    if (getCheckedValue("question" + i) === answers[i]) score += 1; // increment only
+  return score;
+}
+
+$(document).ready(function() {
+  $("form").show();
+  $(".result").hide();
+  $("form").submit(function(event) {
+    event.preventDefault();
+    $("form").hide();
+    $(".result").show();
+    returnScore();
+  });
+});
+
+function returnScore() {
+  // alert("Your score is " + getScore() + "/" + tot);
+  document.getElementById("marks").innerHTML =
+    "Your score is " + "<br>" + getScore() + "/" + tot;
+}
