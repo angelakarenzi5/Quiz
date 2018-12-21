992cfdea681a70ff376ccf431d83fdce0f7a85e2
Angela Uwase <angelakarenzi5@gmail.com>
2018-12-21 19:01:12 +0200

    made conclusion

---
+++ README.md
@@ -0,0 +1,33 @@
+# JavaScript Quiz
+
+#### JavaScript Quiz is the output project of what was taught in week three of prep at Moringa school, Friday, 21,2018
+
+#### By Angela Uwase Karenzi
+
+## Description
+
+The purpose of this project is to be able to show a clear understarting of what was taught in week one. Week one of prep had a lot of content, the content learnt is displayed in the project I created. The application is a basic description of who Angela Uwase Karenzi is, what her hobbies are and then finally tangible proof of what she was taught throughout the week.
+
+## Setup/Installation Requirements
+
+Refer to git clone:https://github.com/angelakarenzi5/Self-portifolio.git for details of code, therefore, you need a code editor.
+You need a browser, to open https://angelakarenzi5.github.io/Self-portifolio/homepage.html in order to view my webpage.
+
+## Known Bugs
+
+No issues whatsoever
+
+## Technologies Used
+
+1.HTML to write my code,
+2.CSS to style my codes,
+3.JavaScript to make my webpages interactive,
+4.Bootstrap.css
+
+## Support and contact details
+
+Incase you run into any issue or have questions, ideas or concerns, feel free to contact me on my email:angelauwase@gmail.com or phone number:+250784584673
+
+### License
+
+Copyright (c) 2018 **Angela Uwase Karenzi**
--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,82 @@
 var count = 0;
-function check() {
-  var questOne = document.quiz.questOne.value;
-  var questTwo = document.quiz.questTwo.value;
-  var questThree = document.quiz.questThree.value;
-  var questFour = document.quiz.questFour.value;
-  var questFive = document.quiz.questFive.value;
-  var questSix = document.quiz.questSix.value;
-  var questSeven = document.quiz.questSeven.value;
-  var questEight = document.quiz.questEight.value;
-  var questNine = document.quiz.questNine.value;
-  var questTen = document.quiz.questTen.value;
-console.log(questOne)
-  if (questOne == "B") {
+function check(
+  questionOne,
+  questionTwo,
+  questionThree,
+  questionFour,
+  questionFive,
+  questionSix,
+  questionSeven,
+  questionEight,
+  questionNine,
+  questionNine,
+  questionTen
+) {
+  if (questionOne == "2") {
     count += 2;
   }
-  if (questTwo == "A") {
+  if (questionTwo == "1") {
     count += 2;
   }
-  if (questThree == "C") {
+  if (questionThree == "3") {
     count += 2;
   }
-  if (questFour == "A") {
+  if (questionFour == "1") {
     count += 2;
   }
-  if (questFive == "B") {
+  if (questionFive == "2") {
     count += 2;
   }
-  if (questSix == "B") {
+  if (questionSix == "2") {
     count += 2;
   }
-  if (questSeven == "D") {
+  if (questionSeven == "4") {
     count += 2;
   }
-  if (questEight == "D") {
+  if (questionEight == "4") {
     count += 2;
   }
-  if (questNine == "A") {
+  if (questionNine == "1") {
     count += 2;
   }
-  if (questTen == "C") {
+  if (questionTen == "3") {
     count += 2;
   }
   return count;
 }
 $(document).ready(function() {
-  $("form").submit(function(event) {
+  $("#myform form").submit(function() {
     event.preventDefault();
-    $("#numbers").text("You got" + count + "/20");
+    var Question1 = $("input[type='radio'][name='questOne']:checked").val();
+    var Question2 = $("input[type='radio'][name='questTwo']:checked").val();
+    var Question3 = $("input[type='radio'][name='questThree']:checked").val();
+    var Question4 = $("input[type='radio'][name='questFour']:checked").val();
+    var Question5 = $("input[type='radio'][name='questFive']:checked").val();
+    var Question6 = $("input[type='radio'][name='questSix']:checked").val();
+    var Question7 = $("input[type='radio'][name='questSeven']:checked").val();
+    var Question8 = $("input[type='radio'][name='questEight']:checked").val();
+    var Question9 = $("input[type='radio'][name='questNine']:checked").val();
+    var Question10 = $("input[type='radio'][name='questTen']:checked").val();
+
+    // console.log(Question1)
+
+    $("#button").hide();
     $("#quiz").hide();
-    $("#numbers").show();
+    $("#numbers").text(
+      "You got " +
+        check(
+          Question1,
+          Question2,
+          Question3,
+          Question4,
+          Question5,
+          Question6,
+          Question7,
+          Question8,
+          Question9,
+          Question10
+        ) +
+        "%."
+    );
   });
 });
